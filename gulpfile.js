805390499974f9bbe4c8c95815c7acc9a34cc92e
renato980 <renatocolan980@gmail.com>
2019-10-16 19:08:52 -0400

    Added .gitIgnore file and tweaked gulpfile.js as well as package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,36 @@
-const { src, dest } = require(`gulp`);
+const { src, dest, watch, series } = require(`gulp`);
 const htmlCompressor = require(`gulp-htmlmin`);
+const htmlValidator = require(`gulp-html`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
 
-let compressHTML = () => {
+let dev = () => {
+    return src(`app/*.html`)
+		.pipe(htmlValidator());	
+};
+
+let build = () => {
     return src(`app/*.html`)
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod/`));
-}
 
-let validateHTML = () => {
-    return src(`html/*.html`)
-        .pipe(dest(`app/`));
-		
-}
+};
 
-gulp dev = validateHTML;
-gulp build = compressHTML;
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 0,
+        server: {      
+            baseDir: [
+                `temp`,
+                `html`
+            ]
+        }
+    });
+
+    watch(`app/*.html`, series(dev)).on(`change`, reload);
+};
+
+exports.dev = dev;
+exports.build = build;
+exports.serve = series(dev, serve);
