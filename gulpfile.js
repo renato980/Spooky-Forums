7ac4d7eb5c764cccd46f060cb560107346e7c34f
renato980 <renatocolan980@gmail.com>
2019-11-20 21:21:14 -0500

    Implemented nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,35 @@ const sass = require(`gulp-sass`);
 const HTMLPreprocessor = require(`gulp-nunjucks-render`);
 const data = require(`gulp-data`);
 const fs = require(`fs`);
+const cache = require('gulp-cache');
 const htmlCompressor = require(`gulp-htmlmin`);
 const htmlValidator = require(`gulp-html`);
 const imageCompressor = require(`gulp-imagemin`);
 const browserSync = require(`browser-sync`);
 const reload = browserSync.reload;
 const TEMP_FOLDER = `.tmp/`;
-const VIEWS_FOLDER = `app/views/`;
+const HTML_FOLDER = `app/views/html`;
 let browserChoice = `default`;
 
+async function clean () {
+    let fs = require(`fs`),
+        i,
+        foldersToDelete = [`.tmp/`];
+
+    for (i = 0; i < foldersToDelete.length; i++) {
+        try {
+            fs.accessSync(foldersToDelete[i], fs.F_OK);
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory was found and will be deleted.\n`);
+            del(foldersToDelete[i]);
+        } catch (e) {
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory does NOT exist or is NOT accessible.\n`);
+        }
+    }
+
+    process.stdout.write(`\n`);
+}
 
 async function safari () {
     browserChoice = `safari`;
@@ -47,36 +67,41 @@ async function allBrowsers () {
 let compileHTML = () => {
     HTMLPreprocessor.nunjucks.configure({watch: false});
 
-    return src(`app/views/html/nunjuck/contact.html`)
+    return src(`app/views/html/*.html`)
 		.pipe(data(function () {
 				return JSON.parse(fs.readFileSync(`./app/models/links-file.json`));
 		}))
 		.pipe(HTMLPreprocessor())
-		.pipe(dest('prod/'));
+		.pipe(dest('.tmp/'));
 };
 
 let dev = () => {
-    return src(`app/views/*.html`)
+    return src(`app/views/html/*.html`)
 		.pipe(htmlValidator());
 };
 
+let imagesDev = () => {
+    return src(`app/views/img/**/*`)
+		.pipe(dest('.tmp/img'));
+};
+
 let build = () => {
-    return src(`app/views/*.html`)
+    return src(`.tmp/*.html`)
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod/`));
 };
 
 let cssDev = () => {
-    return src(`app/views/styles/*.scss`)
+    return src(`app/views/styles/**/*.scss`)
         .pipe(sass({
             outputStyle: `expanded`,
             precision: 10
         }).on(`error`, sass.logError))
-        .pipe(dest(`temp/styles`));
+        .pipe(dest(`.tmp/styles`));
 };
 
 let cssProd = () => {
-    return src(`app/views/styles/*.scss`)
+    return src(`app/views/styles/**/*.scss`)
         .pipe(sass({
             outputStyle: `compressed`,
             precision: 10
@@ -99,24 +124,22 @@ let compressImages = () => {
 
 let serve = () => {
     browserSync({
-        notify: true,
-        reloadDelay: 0,
         server: {
             baseDir: [
-                `TEMP_FOLDER`,
-                `VIEWS_FOLDER`
+                TEMP_FOLDER,
+                HTML_FOLDER
             ]
         }
     });
-		watch([
-				`./app/views/*.html`,
-        `./app/views/css/*.css`,
-        `./app/controllers/*.*`,
-        `./app/controllers/**/**`,
-        `./app/models/*.json`
-			],
-        series(compileHTML)
-    ).on(`change`, reload);
+
+    watch([
+        `app/views/html/*.html`,
+        `app/views/styles/**/**`,
+        `app/controllers/*.*`,
+        `app/controllers/**/**`,
+        `app/models/*.json`
+    ],
+    compileHTMLDev).on(`change`, reload);
 };
 
 exports.safari = series(safari, serve);
@@ -127,9 +150,13 @@ exports.edge = series(edge, serve);
 exports.safari = series(safari, serve);
 exports.allBrowsers = series(allBrowsers, serve);
 exports.compressImages = compressImages;
+exports.imagesDev = imagesDev;
 exports.cssDev = cssDev;
 exports.cssProd = cssProd;
 exports.dev = dev;
 exports.build = build;
+exports.developmentTrack = series(compileHTML, cssDev, imagesDev)
+exports.productionTrack = series(build, cssProd, compressImages)
 exports.compileHTML = compileHTML;
-exports.serve = series(compileHTML, serve);
+exports.serve = series(compileHTML, cssDev, serve);
+exports.clean = clean;
