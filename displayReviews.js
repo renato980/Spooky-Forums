8eba17f7693078a45ce751e78e9cc9fbcaa933e7
renato980 <renatocolan980@gmail.com>
2019-12-17 15:28:32 -0500

    added review score selector to rest of the games

--- displayReviews.js
+++ displayReviews.js
@@ -1,23 +1,14 @@
-const mongoClient = mongoDB.MongoClient;
-const HOST = `localhost`;
-const dbPort = `27017`;
-const dbURL = `mongodb://${HOST}`;
-const dbName = `spooky`;
-const dbCollection = `users`;
-const dbCollection2= 'reviews';
-const PORT = 8080;
-const port = (process.env.PORT || PORT);
+
 
 function displayReviews () {
 			let game = "Alien: Isolation";
-	app.get(`/get-review-from-db`, (req, res) => {
 			let x = 0;
 			let html ="";
 			let totalReviews = db.collection(dbCollection2).count({Title: {game}});
 			if (x != totalReviews){
+				let review = db.collection(dbcollection2.findOne({Message: [x]}));
 				x += 1;
-				html ='<div class="review_block"><h3>Review ' + x +'</h3><p>' + db.Collection(collection2).Message[x] +'</p></div>'
+				html ='<div class="review_block"><h3>Review ' + x +'</h3><p>' + review +'</p></div>'
 			};
 				res.send(html);
-	});
 };
